83e2ce0f778519b159bf5ee9e9e78f916f5cad15
noam mirjani <noamirjani@gmail.com>
2023-01-17 22:17:47 +0200

    add includes directory, change the data base

--- routes/index.js
+++ routes/index.js
@@ -2,56 +2,68 @@ const express = require('express');
 const Cookies = require('cookies');
 const router = express.Router();
 
-const User = require('../models/user.js');
-const db = require("../models/usersList.js");
-
-const keys = ['key']
+const Sequelize = require('sequelize');
+const db = require('../models');
 
 /* GET */
 router.get('/', (req, res) => {
-  res.render('login', { title: 'Login', newRegistered : "" });
+    res.render('login', {title: 'Login', newRegistered: ""});
 });
 
 router.get('/register', (req, res) => {
-  res.render('register', { title: 'register', error: "" });
+    res.render('register', {title: 'register', error: ""});
 });
 
 router.get('/register-passwords', (req, res) => {
-  const cookies = new Cookies(req, res);
-  const cookieData = cookies.get("newUser");
-  if (!cookieData) {
-    res.redirect('/register');
-    return;
-  }
-  res.render('register-passwords', { title: 'register passwords', error: undefined});
+    const cookies = new Cookies(req, res);
+    const cookieData = cookies.get("newUser");
+    if (!cookieData) {
+        res.redirect('/register');
+        return;
+    }
+    res.render('register-passwords', {title: 'register passwords', error: undefined});
 });
 
 /* POST */
 router.post('/', (req, res) => {
-  const {password, confirmPassword} = req.body;
-  if (password !== confirmPassword) {
-    res.render('register-passwords', {title: 'register-passwords', error: "passwords do not match" });
-    return;
-  }
+    const {password, confirmPassword} = req.body;
 
-  const cookies = new Cookies(req, res);
-  const prevData = JSON.parse(cookies.get("newUser"));
-  const user = new User(prevData.email, prevData.firstName, prevData.lastName, password);
+    if (password !== confirmPassword) {
+        res.render('register-passwords', {title: 'register-passwords', error: "passwords do not match"});
+        return;
+    }
 
-  db.enterUser(user);
-  res.render('login', {title: 'Login' , newRegistered : "you are registered"});
+    const cookies = new Cookies(req, res);
+    const {email, firstName, lastName} = JSON.parse(cookies.get("newUser"));
+    let newUser = db.User.build({firstName: firstName, lastName: lastName, email: email, password: password});
+
+    return newUser.save()
+        // db.enterUser(user);
+        .then(() => res.render('login', {title: 'Login', newRegistered: "New user was registered successfully!"}))
+        .catch((err) => {
+            res.render('register-passwords', { title: 'register passwords', error: err});
+        })
 });
 
-router.post('/register-passwords', (req, res) => {
-  const cookies = new Cookies(req, res);
-  const {email, firstName, lastName} = req.body;
-  if (db.isNewUser(email)) {
-    const data = { email, firstName, lastName };
-    cookies.set("newUser", JSON.stringify(data), { maxAge: 30*1000 });
-    res.render('register-passwords', { title: 'register passwords', error: undefined });
-  } else {
-    res.render('register', { title: 'register', error: 'Email already in use'});
-  }
+router.post('/register-passwords', async (req, res) => {
+    const cookies = new Cookies(req, res);
+    const {email, firstName, lastName} = req.body;
+
+    try {
+        const emailExists = await db.User.findOne({where: { email: email } });
+
+        if (emailExists) {
+            res.render('register', { title: 'register', error: 'Email already in use'});
+        }
+        else {
+            const data = {email, firstName, lastName};
+            cookies.set("newUser", JSON.stringify(data), {maxAge: 30 * 1000});
+            res.render('register-passwords', { title: 'register passwords', error: undefined });
+        }
+    }
+    catch (err) {
+        res.render('error', {error: err});
+    }
 });
 
 module.exports = router;
