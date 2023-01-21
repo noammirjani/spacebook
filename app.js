7cd1fc0bcb532ae16715bf493d76b99933462098
noam mirjani <noamirjani@gmail.com>
2023-01-21 19:18:50 +0200

    update session files

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var fs = require('fs')
+const session = require('express-session');
 
 
 var indexRouter = require('./routes/index');
@@ -19,6 +20,14 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
+
+
+//enable sessions
+app.use(session({
+  secret:"secret",
+  resave: false,
+  saveUninitialized: true,
+}));
 
 
 // catch 404 and forward to error handler
